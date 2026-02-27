c64baa1357d5d5f6c40d4101ecad407c57ea6a9b
ravi sharma <rsharma.lnx@gmail.com>
2026-02-27 21:40:54 +0000

    two pointer patter: convergence
    s & f pointer tracks to fit into a condition

---
+++ two_ptr_patterns/t_p_converging.c
@@ -0,0 +1,28 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdint.h>
+
+
+void converging_sum(uint32_t *sample, uint32_t len, uint32_t sum)
+{
+
+
+	uint32_t *s = sample,n = len, *f = sample + n - 1;
+
+
+	for ( uint32_t i = 0; i < n; ++i  ) {
+		if (*s + *f > sum) {f = f - 1; }
+		else if (*s + *f < sum) s += 1;
+	}
+	printf("sum part of %d {%d %d}\n", sum, *s, *f);
+}
+
+int main()
+{
+	/*make sure it is sorted*/
+	uint32_t sum = 10;
+	//uint32_t arr[] = {1, 2 ,4, 6, 8, 11, 15};
+	uint32_t arr[] = {1, 2 ,4, 6, 9, 11, 15};
+	converging_sum(arr, sizeof(arr)/sizeof(arr[0]), sum);
+	return 0;
+}

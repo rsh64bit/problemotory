8fa3e4a36122b72435905825d3368c8462e67526
ravi sharma <rsharma.lnx@gmail.com>
2026-03-02 12:05:15 +0000

    ptr array basic

---
+++ two_ptr_patterns/ptr_array.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+
+
+/*
+ *  int (*ptr)[20]
+ *  int *ptr[20]*/
+
+void check_array(int (*ptr)[20])
+{
+    int *x = (int*)ptr;
+    printf("data check %p %d %d\n", ptr, x[0], *ptr[0]);
+}
+
+void check_array_of_ptr(int *ptr[20])
+{
+    check_array((int(*)[20])ptr[0]);
+}
+
+
+int main()
+{
+
+    int arr[20] = {1, 2, 4};
+    int brr[20] = {4, 5, 6};
+
+    /*both are same*/
+    check_array(&arr);
+    check_array(arr);
+
+    int *ptr_array[20] = {arr, brr}; 
+
+    check_array_of_ptr(ptr_array);
+
+    return 0;
+}

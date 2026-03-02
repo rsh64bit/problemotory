17351fada47df14b5cd625a7208eab8332f487c8
ravi sharma <rsharma.lnx@gmail.com>
2026-03-02 12:06:01 +0000

    clag formatting

--- two_ptr_patterns/ptr_array.c
+++ two_ptr_patterns/ptr_array.c
@@ -1,7 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdint.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
 
 /*
  *  int (*ptr)[20]
@@ -9,29 +8,27 @@
 
 void check_array(int (*ptr)[20])
 {
-    int *x = (int*)ptr;
-    printf("data check %p %d %d\n", ptr, x[0], *ptr[0]);
+	int *x = (int *)ptr;
+	printf("data check %p %d %d\n", ptr, x[0], *ptr[0]);
 }
 
 void check_array_of_ptr(int *ptr[20])
 {
-    check_array((int(*)[20])ptr[0]);
+	check_array((int(*)[20])ptr[0]);
 }
-
 
 int main()
 {
+	int arr[20] = { 1, 2, 4 };
+	int brr[20] = { 4, 5, 6 };
 
-    int arr[20] = {1, 2, 4};
-    int brr[20] = {4, 5, 6};
+	/*both are same*/
+	check_array(&arr);
+	check_array(arr);
 
-    /*both are same*/
-    check_array(&arr);
-    check_array(arr);
+	int *ptr_array[20] = { arr, brr };
 
-    int *ptr_array[20] = {arr, brr}; 
+	check_array_of_ptr(ptr_array);
 
-    check_array_of_ptr(ptr_array);
-
-    return 0;
+	return 0;
 }

203bef881a3bddb2e0441a2573c251777e33d046
ravi sharma <rsharma.lnx@gmail.com>
2026-03-04 12:37:26 +0000

    signed mystery
    formatting fixed and width changes from 8 to 2

--- two_complement/signed.c
+++ two_complement/signed.c
@@ -2,40 +2,29 @@
 #include<stdlib.h>
 #include<stdint.h>
 
-
 unsigned char map[] = {'0', '1', '2', '3' , '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'}; 
 
 /*num range max is 0xff = 255*/
 void int2hex(int num)
 {
-
     int i = 0;
+    /*byte 8 bit wide*/
     uint8_t hex[2] = {};
-    uint8_t real_hex[8] = "\0";
+    uint8_t real_hex[2] = {};
 
     while (num != 0) {
         hex[i++] = map[num%16];
         num =  num / 16;
     }
-
     real_hex[0] = hex[1];
     real_hex[1] = hex[0];
-
-
-    //printf("0x%s %d\n", hex, strlen(hex));
     printf("0x%s\n", real_hex);
-
-
-
 }
-
 
 int main()
 {
     /*x = -128 to 127*/
     char x = 0xf9, c = 0;
-
-
 
     //for (uint32_t i = 0; i <= 255; ++i) printf(" %2d ", (char)i);
     /* signed from a hex 

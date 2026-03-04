0fb90d237c96afbbeda3947ae69f8d002b378d76
ravi sharma <rsharma.lnx@gmail.com>
2026-03-04 10:58:25 +0000

    bitwise: count number of bits
    1.count by clearing lowest bits
     value = value & (value - 1) clears the lowest bits
     value = 0b0000 1100 -> , 0b0000 1000, 0b0000 0000
    
    2.count by finding the lowers bit
    value- = value & -value
    value  = 0b0000 1100 -> 0b0000 1000 -> 0b0000 0000
    
    3.count by checking and shifting by 1

---
+++ two_complement/two_compl.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdint.h>
+
+#define TEST_VAL 0x0f
+int main()
+{
+
+    char x, y, z;
+    int pos = 0;
+    int count = 0;
+    /*
+     * x = 0x05 = 0000 0101
+     * -x = two's(x) = 1111 1010 + 1 = 1111 1011 = 0xfb = 27
+     * x = 0x04 = 0000 0100
+     * -x = 1111 1011 + 1 = 1111 1100 -> add 1 + 1 and pass the carry  
+     * Q: How 0xfb become -5 for signed integer?
+     */
+
+    x = y = z = TEST_VAL;
+
+
+    //printf("signed x %x %d\n", -x, 0xfb);
+
+    while (y) {
+        if ( y & 0x1) ++count;
+        y = y >> 1;
+    }
+
+    printf("method 1 count %d\n", count);
+
+    /*find the lowest bit set*/
+    /*subtract that value from the original removes that*/
+    count = 0;
+    while (x) {
+        x -= x & -x;
+        ++count;
+    }
+    printf("method 2 count %d\n", count);
+
+    count = 0;
+
+
+    /*Brian Kernighan’s bit-count algorithm*/
+    /*clear lowest bit. update value and keep clearing*/
+    while (z) {
+        z &= (z - 1);
+        ++count;
+    }
+    printf("method 3 count %d\n", count);
+
+    //clear lowest set bit quickly
+
+    char l = 0x0a, _l = l;
+
+    _l &= (_l - 1);
+
+    printf("lowest bit cleared %x %x\n", l, _l);
+
+    char __l = l;
+
+    printf("find the first %d nth bit set -> %x\n", (__l & -__l) - 1, l);
+
+    return 1;
+};
